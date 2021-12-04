0b870bd47de5ca10581bbf271503db5e156c3f7e
Nguyễn Văn Khánh <57382524+COCC390@users.noreply.github.com>
2021-12-04 13:08:17 +0700

    add create to do and fix note bug

--- app/src/main/java/com/bc/secretnoteandtodo/view/NotesAdapter.java
+++ app/src/main/java/com/bc/secretnoteandtodo/view/NotesAdapter.java
@@ -1,14 +1,20 @@
 package com.bc.secretnoteandtodo.view;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bc.secretnoteandtodo.CreateNewNote;
 import com.bc.secretnoteandtodo.R;
+import com.bc.secretnoteandtodo.database.DatabaseHelper;
 import com.bc.secretnoteandtodo.database.model.Note;
 
 import java.text.ParseException;
@@ -16,66 +22,78 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.MyViewHolder> {
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.MyViewHolder>
+{
 
-    private Context context;
-    private List<Note> notesList;
+    private List<Note> noteList;
+    private AllNotes allNotesActivity;
+    private DatabaseHelper db;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView note;
-        public TextView dot;
-        public TextView timestamp;
-
-        public MyViewHolder(View view) {
-            super(view);
-            note = view.findViewById(R.id.note);
-            dot = view.findViewById(R.id.dot);
-            timestamp = view.findViewById(R.id.timestamp);
-        }
+    public NotesAdapter(DatabaseHelper db, AllNotes allNotes)
+    {
+        this.db = db;
+        this.allNotesActivity = allNotes;
     }
 
-
-    public NotesAdapter(Context context, List<Note> notesList) {
-        this.context = context;
-        this.notesList = notesList;
-    }
-
-    @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.note_list_row, parent, false);
-
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_list_row, parent, false);
         return new MyViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        Note note = notesList.get(position);
+    public void onBindViewHolder(@NonNull final NotesAdapter.MyViewHolder holder, int position)
+    {
+        db.openDatabase();
 
+        final Note note = noteList.get(position);
         holder.note.setText(note.getContent());
-
-        // Displaying dot from HTML character code
-        // holder.dot.setText(Html.fromHtml("&#8226;"));
-
-        // Formatting and displaying timestamp
-        // holder.timestamp.setText(formatDate(note.getTimestamp()));
     }
 
     @Override
-    public int getItemCount() {
-        return notesList.size();
+    public int getItemCount()
+    {
+        return noteList.size();
     }
 
-    private String formatDate(String dateStr) {
-        try {
-            SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Date date = fmt.parse(dateStr);
-            SimpleDateFormat fmtOut = new SimpleDateFormat("MMM d");
-            return fmtOut.format(date);
-        } catch (ParseException e) {
+    public Context getContext()
+    {
+        return allNotesActivity;
+    }
 
+    public void setNotes(List<Note> noteList)
+    {
+        this.noteList = noteList;
+        notifyDataSetChanged();
+    }
+
+    public void deleteItem(int position)
+    {
+        Note note = noteList.get(position);
+        db.deleteNote(note.getId());
+        noteList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void editItem(int position)
+    {
+        Note note = noteList.get(position);
+        Bundle bundle = new Bundle();
+        bundle.putInt("id", note.getId());
+        bundle.putString("content", note.getContent());
+        CreateNewNote fragment = new CreateNewNote();
+        fragment.setArguments(bundle);
+        fragment.show(allNotesActivity.getSupportFragmentManager(), CreateNewNote.TAG);
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView note;
+
+        MyViewHolder(View view)
+        {
+            super(view);
+            note = view.findViewById(R.id.noteTextView);
         }
-
-        return "";
     }
 }
